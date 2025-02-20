5db34fe75039e388f1f3f9d25ee9dfdb3a76257f
vinay-scienaptic <vinay.pottabathini@scienaptic.com>
2025-02-20 18:23:15 +0530

    updated mail config

--- mail_confg.py
+++ mail_confg.py
@@ -23,21 +23,49 @@ MAIL_FROM = "techsupport@scienaptic.com"
 MAIL_TO = "vinay.pottabathini@scienaptic.com"
 MAIL_CC = ""
 
-def send_alert():
-    """to send alert
+# def send_alert():
+#     """to send alert
 
-    Args:
-        data (_type_): _description_
-    """
-    multipart_message = MIMEMultipart()
-    multipart_message["From"] = MAIL_FROM
-    multipart_message["To"] = ", ".join(MAIL_TO)
-    multipart_message["Cc"] = ", ".join(MAIL_CC)
-    multipart_message["Subject"] = "Client Request"
-    msg = "Hello THis is email text"
-    multipart_message.attach(MIMEText(msg, "html"))
-    smtplib_server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-    smtplib_server.starttls()
-    smtplib_server.login(str(MAIL_USER), str(MAIL_USER_PASSWORD))
-    smtplib_server.send_message(multipart_message)
-    smtplib_server.quit()
+#     Args:
+#         data (_type_): _description_
+#     """
+#     multipart_message = MIMEMultipart()
+#     multipart_message["From"] = MAIL_FROM
+#     multipart_message["To"] = ", ".join(MAIL_TO)
+#     multipart_message["Cc"] = ", ".join(MAIL_CC)
+#     multipart_message["Subject"] = "Client Request"
+#     msg = "Hello THis is email text"
+#     multipart_message.attach(MIMEText(msg, "html"))
+#     smtplib_server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+#     smtplib_server.starttls()
+#     smtplib_server.login(str(MAIL_USER), str(MAIL_USER_PASSWORD))
+#     smtplib_server.send_message(multipart_message)
+#     smtplib_server.quit()
+
+
+
+
+import smtplib
+from email.message import EmailMessage
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = "techsupport@scienaptic.com"
+EMAIL_PASSWORD = "ojltbktqfpneztep"
+
+def send_alert():
+    msg = EmailMessage()
+    msg["Subject"] = "Test Email"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = "srikanth.k@scienaptic.com"
+    msg.set_content("This is a test email.")
+
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()  # Secure the connection
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)  # Authenticate
+        server.send_message(msg)  # Send email
+        server.quit()
+        print("Email sent successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
